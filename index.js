4ca86a769d21c9fc2b8cda5d46c7e58c83a7ec14
bonveeee <bonochieng@gmail.com>
2022-06-13 22:45:25 +0300

    commit tree 4fd616b94c4de62546efacc83ffb7d9c594134e8

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  for (String of tutorials){
-    tutorials.map(String.titleCased);
-    return tutorials
+
+  const titleCased = () => {
+    return tutorials.map((sentence) => {
+      const sentenceSplit = sentence.split(" ");
+      const sentenceSplitUppercase = sentenceSplit.map((sentenceSplit) => sentenceSplit.charAt(0).toUpperCase() +sentenceSplit.slice(1));
+      const result = sentenceSplitUppercase.join(" ");
+      return result;
+    });
   }
-  }
+  
